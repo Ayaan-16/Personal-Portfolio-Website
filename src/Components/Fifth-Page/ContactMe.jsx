0e8fb4dc77f663102e4c165ed02efc58bbb01f6f
Ayaan Noman <165467596+Ayaan-16@users.noreply.github.com>
2024-06-27 14:06:16 +0500

    Update ContactMe.jsx

--- src/Components/Fifth-Page/ContactMe.jsx
+++ src/Components/Fifth-Page/ContactMe.jsx
@@ -5,7 +5,7 @@ function ContactMe() {
     return (
         <div className='main-container'>
             <h1 className='heading'>Contact Me</h1>
-            <p className='text'>I'm currently looking for full-time Front End React Web Developer Job. If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at : <a href="mailto:ayaannoamn2112@gmail.com" style={{textDecoration: 'none'}}>ayaannoamn2112@gmail.com</a></p>
+            <p className='text'>I'm currently working as a Front End React Web Developer Freelancer. If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at : <a href="mailto:ayaannoamn2112@gmail.com" style={{textDecoration: 'none'}}>ayaannoamn2112@gmail.com</a></p>
         </div>
     );
 }
